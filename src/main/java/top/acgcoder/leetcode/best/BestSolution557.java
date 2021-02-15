2ffcb56eb67cc1170447008913ed3afe83e3a767
ACGCoder <ACGCoder@163.com>
2021-02-15 20:05:18 +0800

    Add best solution 557

---
+++ src/main/java/top/acgcoder/leetcode/best/BestSolution557.java
@@ -0,0 +1,26 @@
+package top.acgcoder.leetcode.best;
+
+import top.acgcoder.leetcode.Solution557;
+
+public class BestSolution557 implements Solution557 {
+    @Override
+    public String reverseWords(String s) {
+        StringBuilder ret = new StringBuilder();
+        int length = s.length();
+        int i = 0;
+        while (i < length) {
+            int start = i;
+            while (i < length && s.charAt(i) != ' ') {
+                i++;
+            }
+            for (int p = start; p < i; p++) {
+                ret.append(s.charAt(start + i - 1 - p));
+            }
+            while (i < length && s.charAt(i) == ' ') {
+                i++;
+                ret.append(' ');
+            }
+        }
+        return ret.toString();
+    }
+}
--- src/test/java/top/acgcoder/leetcode/me/Solution557Test.java
+++ src/test/java/top/acgcoder/leetcode/me/Solution557Test.java
@@ -2,12 +2,14 @@ package top.acgcoder.leetcode.me;
 
 import org.junit.jupiter.api.Test;
 import top.acgcoder.leetcode.Solution557;
+import top.acgcoder.leetcode.best.BestSolution557;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution557Test {
 
-    private final Solution557 solution557 = new MySolution557();
+//    private final Solution557 solution557 = new MySolution557();
+    private final Solution557 solution557 = new BestSolution557();
 
     @Test
     void reverseWords() {
